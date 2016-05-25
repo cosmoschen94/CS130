cddf4a248ff80e8e8cfa51ae626e9ed85a117a95
Siyu Chen <Siyu Chen>
2016-05-24 20:22:49 -0700

    Added hover to update articles

--- js/amchart.js
+++ js/amchart.js
@@ -29,7 +29,7 @@ var chart = AmCharts.makeChart("chartdiv", {
     "valueField": "positive",
     "clustered": false,
     "columnWidth": 0.5,
-    "legendValueText": "$[[value]]",
+    "legendValueText": "[[value]]",
     "balloonText": "[[title]]<br/><b style='font-size: 130%'>[[value]]</b>"
   }, {
     "id": "g4",
@@ -42,7 +42,7 @@ var chart = AmCharts.makeChart("chartdiv", {
     "valueField": "negative",
     "clustered": false,
     "columnWidth": 0.3,
-    "legendValueText": "$[[value]]M",
+    "legendValueText": "[[value]]",
     "balloonText": "[[title]]<br/><b style='font-size: 130%'>[[value]]</b>"
   }, {
     "id": "g1",
@@ -58,6 +58,7 @@ var chart = AmCharts.makeChart("chartdiv", {
     "title": "Stock Price",
     "useLineColorForBulletBorder": true,
     "valueField": "price",
+    "legendValueText": "$[[value]]",
     "balloonText": "[[title]]<br/><b style='font-size: 130%'>$[[value]]</b>"
   }],
   "chartScrollbar": {
@@ -99,120 +100,5 @@ var chart = AmCharts.makeChart("chartdiv", {
   "export": {
    "enabled": true
   },
-  "dataProvider": [{
-    "date": "2016-04-04",
-    "price": 55.43,
-    "positive": 5,
-    "negative": 8
-  }, {
-    "date": "2016-04-05",
-    "price": 54.56,
-    "positive": 4,
-    "negative": 6
-  }, {
-    "date": "2016-04-06",
-    "price": 55.12,
-    "positive": 5,
-    "negative": 2
-  }, {
-    "date": "2016-04-07",
-    "price": 54.46,
-    "positive": 8,
-    "negative": 9
-  }, {
-    "date": "2016-04-08",
-    "price": 54.42,
-    "positive": 9,
-    "negative": 6
-  }, {
-    "date": "2016-04-11",
-    "price": 54.31,
-    "positive": 3,
-    "negative": 5
-  }, {
-    "date": "2016-04-12",
-    "price": 54.65,
-    "positive": 5,
-    "negative": 7
-  }, {
-    "date": "2016-04-13",
-    "price": 55.35,
-    "positive": 7,
-    "negative": 6
-  }, {
-    "date": "2016-04-14",
-    "price": 55.36,
-    "positive": 9,
-    "negative": 5
-  }, {
-    "date": "2016-04-15",
-    "price": 55.65,
-    "positive": 5,
-    "negative": 8
-  }, {
-    "date": "2016-04-18",
-    "price": 56.46,
-    "positive": 4,
-    "negative": 8
-  }, {
-    "date": "2016-04-19",
-    "price": 56.39,
-    "positive": 3,
-    "negative": 4
-  }, {
-    "date": "2016-04-20",
-    "price": 55.59,
-    "positive": 5,
-    "negative": 7
-  }, {
-    "date": "2016-04-21",
-    "price": 55.78,
-    "positive": 5,
-    "negative": 8
-  }, {
-    "date": "2016-04-22",
-    "price": 51.78,
-    "positive": 4,
-    "negative": 7
-  }, {
-    "date": "2016-04-25",
-    "price": 52.11,
-    "positive": 4,
-    "negative": 8
-  }, {
-    "date": "2016-04-26",
-    "price": 51.44,
-    "positive": 3,
-    "negative": 4
-  }, {
-    "date": "2016-04-27",
-    "price": 50.94,
-    "positive": 5,
-    "negative": 7
-  }, {
-    "date": "2016-04-28",
-    "price": 49.90,
-    "positive": 5,
-    "negative": 8
-  }, {
-    "date": "2016-04-29",
-    "price": 49.87,
-    "positive": 4,
-    "negative": 7
-  }, {
-    "date": "2016-05-02",
-    "price": 50.61,
-    "positive": 5,
-    "negative": 7
-  }, {
-    "date": "2016-05-03",
-    "price": 49.78,
-    "positive": 5,
-    "negative": 8
-  }, {
-    "date": "2016-05-04",
-    "price": 49.87,
-    "positive": 4,
-    "negative": 7
-  }]
+  "dataProvider": []
 });
